767b5a409b329a730d6ba14f3956ca0cb869a3dc
kenenx <kenen1996@hotmail.co.uk>
2023-04-19 17:47:57 +0100

    results is partially working now

--- assets/result.js
+++ assets/result.js
@@ -8,14 +8,14 @@ async function fetchScoreData(user) {
     await fetch(`https://geo-trumps-api.onrender.com/players/${user}`, options)
     .then(resp => resp.json())
     .then(data => {
-        // console.log(data)
+         console.log(data)
         const userdata = {
             user: data.user,
             score: data.score
         };
         const scoreElement = document.getElementById("player-score")
         scoreElement.textcontent = userdata['score']
-        // console.log(userdata["score"])
+         console.log(userdata["score"])
     })
 }
 
@@ -35,28 +35,28 @@ async function fetchHighScores(){
 
         const name1Element = document.getElementById("name1")
         const score1Element = document.getElementById("score1")
-        name1Element.textcontent = data[0]["user"]
-        score1Element.textcontent = data[0]["score"]
+        name1Element.innerHTML = data[0]["user"]
+        score1Element.innerHTML = data[0]["score"]
 
         const name2Element = document.getElementById("name2")
         const score2Element = document.getElementById("score2")
-        name2Element.textcontent = data[1]["user"]
-        score2Element.textcontent = data[1]["score"]
+        name2Element.innerHTML = data[1]["user"]
+        score2Element.innerHTML = data[1]["score"]
 
         const name3Element = document.getElementById("name3")
         const score3Element = document.getElementById("score3")
-        name3Element.textcontent = data[2]["user"]
-        score3Element.textcontent = data[2]["score"]
+        name3Element.innerHTML = data[2]["user"]
+        score3Element.innerHTML = data[2]["score"]
 
         const name4Element = document.getElementById("name4")
         const score4Element = document.getElementById("score4")
-        name4Element.textcontent = data[3]["user"]
-        score4Element.textcontent = data[3]["score"]
+        name4Element.innerHTML = data[3]["user"]
+        score4Element.innerHTML = data[3]["score"]
 
         const name5Element = document.getElementById("name5")
         const score5Element = document.getElementById("score5")
-        name5Element.textcontent = data[0]["user"]
-        score5Element.textcontent = data[0]["score"]
+        name5Element.innerHTML = data[4]["user"]
+        score5Element.innerHTML = data[4]["score"]
 
     })
 }
